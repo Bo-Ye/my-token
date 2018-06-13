b68ac43be47ea0919a2e2630c399953bf3dab4f6
Bo Ye <boy.e.computer.1982@outlook.com>
2018-06-14 01:00:10 +1200

    Test burn function

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 var MyToken = artifacts.require("./MyToken.sol");
 var TokenERC20 = artifacts.require("./TokenERC20.sol");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
   deployer.deploy(MyToken, 100);
-  deployer.deploy(TokenERC20, 100, "Token ERC20", "Token ERC20");
+  deployer.deploy(TokenERC20, 100, "Token ERC20", "Token ERC20", {from: accounts[0]});
 };
---
+++ test/tokenerc20.js
@@ -0,0 +1,19 @@
+var TokenERC20 = artifacts.require("TokenERC20");
+
+contract('TokenERC20', function(accounts) {
+  it("should be 90 * 10 ** 18 tokens after burning 10 * 10 ** 18 tokens", function() {
+    var tokenERC20;
+    return TokenERC20.deployed().then(function(instance) {
+      tokenERC20 = instance;
+      return tokenERC20.burn(10 * 10 ** 18, {from: accounts[0]});
+    }).then(function(success){
+      return tokenERC20.balanceOf.call(accounts[0]);
+    }).then(function(balance){
+      assert.equal(balance.toNumber(), 90 * 10 ** 18, "Owner should have 90 * 10 ** 18 tokens after burning 10 * 10 ** 18 tokens");
+    }).then(function(){
+      return tokenERC20.totalSupply.call();
+    }).then(function(totalSupply){
+      assert.equal(totalSupply.toNumber(), 90 * 10 ** 18, "Total supply should be 90 * 10 ** 18 tokens after burning 10 * 10 ** 18 tokens");
+    });
+  });
+});
